e230d3ed9f6c5c2f593965a96b629198bbfdfe2d
0908-yukito <tourentiancun992@gmail.com>
2020-06-01 18:53:15 +0900

    Vue2

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false
+const config = {
+  apikey:'AIzaSyAgRHX9htGL-9B3OXqoH3dxnPhXTQ2Fn1U',
+  projectId:'text-4f266',
+  authDomain:'text-4f266.firebaseapp.com',
+  databaseURL:'https://text-4f266.firebaseio.com',
+  storageBucket:'text-4f266.appspot.com'
+}
+
+
+
+
+firebase.initializeApp(config);
+
+export default firebase;
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
